8ed9be8e87fb2f631fc0a63705898ddf680537fb
Varun Arora <aroravarun@baidu.com>
2019-01-03 17:00:26 -0800

    Fix accidental API lang config

--- portal/portal/config/en/conf.py
+++ portal/portal/config/en/conf.py
@@ -97,7 +97,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'api/*', '**/*_cn*', '*_cn*', '**/*.cn*', '*.cn*', '**/*hidden.*']
+exclude_patterns = ['_build', '**/*_cn*', '*_cn*', '**/*.cn*', '*.cn*', '**/*hidden.*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

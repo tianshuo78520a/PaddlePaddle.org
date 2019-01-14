7ad2d2c34e04474d0b0b5aa2f691abce5e4aaaaf
Varun Arora <aroravarun@baidu.com>
2019-01-14 14:43:39 -0800

    Fix search index page, and catch errors in search rather than shutting server

--- portal/search-server.js
+++ portal/search-server.js
@@ -11,32 +11,36 @@ function onRequest(request, response) {
     var parsed_url = url.parse(request.url);
         path = parsed_url.pathname;
 
-    if (path === '/search' && request.method == 'GET') {
-        var items = [],
-            parsedQuery = qs.parse(parsed_url.query),
-            version = parsedQuery.version,
-            language = parsedQuery.lang;
+    try {
+        if (path === '/search' && request.method == 'GET') {
+            var items = [],
+                parsedQuery = qs.parse(parsed_url.query),
+                version = parsedQuery.version,
+                language = parsedQuery.lang;
 
-        if (!indexes[language].hasOwnProperty(version)){
-            var index = require([indexesPath, language, version, 'index'].join('/'));
+            if (!indexes[language].hasOwnProperty(version)){
+                var index = require([indexesPath, language, version, 'index'].join('/'));
 
-            indexes[language][version] = {
-                index: index,
-                indexPathMap: require([indexesPath, language, version, 'toc'].join('/')),
-                idx: lunr.Index.load(index)
-            };
+                indexes[language][version] = {
+                    index: index,
+                    indexPathMap: require([indexesPath, language, version, 'toc'].join('/')),
+                    idx: lunr.Index.load(index)
+                };
+            }
+
+            var currentIndex = indexes[language][version];
+
+            currentIndex.idx.search(parsedQuery.q).map((result) => {
+                items.push(currentIndex.indexPathMap[result.ref]);
+            });
+
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.write(JSON.stringify({ results: items }));
+            response.end();
         }
 
-        var currentIndex = indexes[language][version];
-
-        currentIndex.idx.search(parsedQuery.q).map((result) => {
-            items.push(currentIndex.indexPathMap[result.ref]);
-        });
-
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({ results: items }));
-        response.end();
-    }
+    // NOTE(Varun): At some point log/report this.
+    } catch (e){}
 }
 
 http.createServer(onRequest).listen(8888);

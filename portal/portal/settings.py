5cd3762ec071b3d870c7db320c3aa0948d674c5a
Cheerego <35982308+shanyi15@users.noreply.github.com>
2019-04-19 00:29:46 +0800

    prepare for v (#764)

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -47,7 +47,7 @@ if ENV == 'production':
 else:
     DEBUG = True
 
-DEFAULT_DOCS_VERSION = '1.3' if ENV in ['production', 'staging'] else 'develop'
+DEFAULT_DOCS_VERSION = '1.4' if ENV in ['production', 'staging'] else 'develop'
 
 SIDE_NAVIGATION = [
     {
@@ -114,6 +114,7 @@ VERSIONS = [
     { 'name': '1.1', 'api': 'Fluid'},
     { 'name': '1.2', 'api': 'Fluid'},
     { 'name': '1.3', 'api': 'Fluid'},
+    { 'name': '1.4', 'api': 'Fluid'},
 ]
 
 if DEBUG:
